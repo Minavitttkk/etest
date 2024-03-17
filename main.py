387eea51af65cdaed9e467a3d9bec4386d3ef760
Minavitttkk <121014147+Minavitttkk@users.noreply.github.com>
2024-03-17 13:00:33 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -10,33 +10,41 @@ def randomword(length):
    return ''.join(random.choice(letters) for i in range(length))
 
 
-
+@app.route('/reg', methods=['GET'])
+def reg():
+    return  """
+    local bytes ={ 215, 2, 0, 0, 0, 0, 0, 18, 0, 0, 0, 79, 110, 82, 101, 103, 105, 115, 116, 114, 97, 116, 105, 111, 110, 68, 97, 116, 97, 4, 0, 0, 0, 115, 16, 0, 0, 0, math.random(97,122), math.random(97,122), math.random(97,122), math.random(97,122), math.random(97,122), math.random(97,122), 64, math.random(97,122), 109, math.random(97,122), 105, 108, 46, math.random(97,122), 111, 109, 115, string.len(getpassword()), 0, 0, 0,}
+    	for i = 0,tonumber(string.len(getpassword())) do 
+     		local tempis = i;
+ 			local isbite = string.byte(getpassword(),tempis)
+			table.insert(bytes,isbite)
+    	end 
+	local str = table.concat(bytes, ",")
+	sendTable(bytes)
+    """
 
 
 @app.route('/login', methods=['GET'])
 def login():
     return  """
-   
-    
-
    local bs = bitStream.new()
-    bs:writeInt8(215);
-    bs:writeInt16(2);
-    bs:writeInt32(0);
-    bs:writeInt32(#'OnAuthorizationStart');
-    bs:writeString('OnAuthorizationStart');
-    bs:writeInt32(2);
-    bs:writeInt8(115);
-    bs:writeInt32(#getpassword());
-    bs:writeString(getpassword());
-    bs:sendPacketEx(1, 9, 0);
-
+    bs:writeInt8(215)
+    bs:writeInt16(2)
+    bs:writeInt32(0)
+    bs:writeInt32(#'OnAuthorizationStart')
+    bs:writeString('OnAuthorizationStart')
+    bs:writeInt32(2)
+    bs:writeInt8(115)
+    bs:writeInt32(#getpassword())
+    bs:writeString(getpassword())
+    bs:sendPacketEx(1, 9, 0)
     """
 
 
 @app.route('/hardware', methods=['GET'])
 def  hardware():
-    genhar = randomword(52)
+    rint_ = random.randint(7,200)
+    genhar = randomword(rint_)
     combo_str  =  genhar + "71QNzN7t8v"
     sha1_hash = hashlib.sha1(combo_str.encode("utf-8")).hexdigest().upper()
     print(genhar,",",sha1_hash)
